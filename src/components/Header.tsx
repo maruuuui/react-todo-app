eac25fca15f83389d73a01010bb907c700901f7d
tmp <hogehoge@example.com>
2022-12-27 09:52:11 +0900

    ナビゲーションバーに各画面へのリンクを追加

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,10 +6,15 @@ type HeaderProps = {
   setShouldFetchToDoData: () => void
 }
 function Header(props: HeaderProps) {
+  const path = window.location.pathname
   return (
     <Navbar bg="light">
       <Container fluid>
         <Navbar.Brand className="mb-0">ToDoアプリ</Navbar.Brand>
+        <Nav className="me-auto" variant="tabs" defaultActiveKey={path}>
+          <Nav.Link href="/">一覧表示</Nav.Link>
+          <Nav.Link href="/calendar">カレンダー表示</Nav.Link>
+        </Nav>
         <Nav.Item>
           <Button variant="success" onClick={props.openModal}>
             新規作成
